9d308cc1fb7005a66b83e8fea1cd9e0b397fe551
Alex Ellison <acelli@mit.edu>
2020-05-23 23:15:47 -0500

    updated example event odds

--- src/scripts/get_probabilities.ts
+++ src/scripts/get_probabilities.ts
@@ -3,48 +3,23 @@ import * as mongoose from "mongoose";
 require('dotenv').config();
 
 
-async function getWinProbabilities(event) {
+
+async function getWinOdds(event) {
     let name_to_id = await getNicknameToId();
-    let data = await getSortedEloList(event, name_to_id);
-    let unsorted_names = data["names"];
-    let sorted_elos = data["sorted_elos"];
-    let elo_to_lane = data["elo_to_lane"]
-    let sorted_prob_list = await calculateProbabilities(sorted_elos);
+    let result = await getSortedEloList(event, name_to_id);
+    let swimmer_elos = result["sorted_elos"];
+    let elo_to_place = result["elo_to_place"];
+    let sorted_prob_list = await calculateProbabilities(swimmer_elos);
     let prob_list = new Array(sorted_prob_list.length - 1).fill(0);
-    for (let k=0;k<sorted_elos.length;k++) {
-        let pos = elo_to_lane[sorted_elos[k]];
+    for (let k=0;k<swimmer_elos.length;k++) {
+        let pos = elo_to_place[swimmer_elos[k]];
         prob_list[pos-1] = sorted_prob_list[k]
     }
-    console.log("Names: ", unsorted_names);
-    console.log("Probabilities: ", prob_list);
-}
-
-async function getSortedEloList(event, name_to_id) {
-    let elo_list = [];
-    let elo_to_lane = {};
-    let names = [];
-    for (let athlete of event) {
-        let athlete_name = athlete[0].split(" ");
-        let half = Math.ceil(athlete_name.length/2);
-        let id = await getSwimmerID(athlete_name.slice(half, athlete_name.length).join(" "), athlete_name.slice(0, half).join(" "), name_to_id);
-        let swimmer = await SwimmerModel.findById(id);
-        if (swimmer == null) {
-            console.log(athlete_name);
-            console.log("DNE");
-        } else {
-            elo_list.push(swimmer.elo);
-            elo_to_lane[swimmer.elo] = athlete[1];
-            names.push(swimmer.firstName + " " + swimmer.lastName);
-        }
-    }
-    let result = {}
-    result["unsorted_elos"] = elo_list;
-    result["unsorted_names"] = names;
-    let sorted_elos = [...elo_list];
-    sorted_elos.sort(function(a, b){return b-a});
-    result["sorted_elos"] = sorted_elos;
-    result["elo_to_lane"] = elo_to_lane;
-    return result;
+    console.log("Event: ", event);
+    console.log("Probs: ", prob_list);
+    let odds_list = await prob_to_odds(prob_list);
+    console.log("Odds: ", odds_list);
+    return odds_list;
 }
 
 async function getNicknameToId() {
@@ -61,10 +36,29 @@ async function getNicknameToId() {
     return nick_to_id
 }
 
-// Fixes errors with the names being formatted different
-async function getSwimmerID(first_name, last_name, name_to_id) {
-    let name = first_name.toLowerCase() + " " + last_name.toLowerCase();
-    return name_to_id[name]
+async function getSortedEloList(event, name_to_id) {
+    let elo_list = [];
+    let elo_to_place = {};
+    for (let athlete of event) {
+        let athlete_name = athlete[0].split(" ");
+        let half = Math.ceil(athlete_name.length/2);
+        let id = await getSwimmerID(athlete_name.slice(half, athlete_name.length).join(" "), athlete_name.slice(0, half).join(" "), name_to_id);
+        let swimmer = await SwimmerModel.findById(id);
+        if (swimmer == null) {
+            console.log(athlete_name);
+            console.log("DNE");
+        } else {
+            elo_list.push(swimmer.elo);
+            elo_to_place[swimmer.elo] = athlete[1];
+        }
+    }
+    let result = {}
+    result["unsorted_elos"] = elo_list;
+    let sorted_elos = [...elo_list];
+    sorted_elos.sort(function(a, b){return b-a});
+    result["sorted_elos"] = sorted_elos;
+    result["elo_to_place"] = elo_to_place;
+    return result;
 }
 
 async function calculateProbabilities(swimmer_elos) {
@@ -84,7 +78,12 @@ async function calculateProbabilities(swimmer_elos) {
     return probability_list;
 }
 
-async function elo_to_odds(prob_list) {
+async function getSwimmerID(first_name, last_name, name_to_id) {
+    let name = first_name.toLowerCase() + " " + last_name.toLowerCase();
+    return name_to_id[name]
+}
+
+async function prob_to_odds(prob_list) {
     let odds_list = [];
     for (let prob of prob_list) {
         odds_list.push(1/prob);
@@ -92,8 +91,9 @@ async function elo_to_odds(prob_list) {
     return odds_list
 }
 
-let e = [["Dressel Caeleb", 1], ["Manaudou Florent", 2], ["Adrian Nathan", 3], ["Proud Ben", 4]]
-getWinProbabilities(e);
+
+let e = [["Dressel Caeleb", 1], ["Shields Tom", 2], ["Switkowski Jan", 3], ["Conger Jack", 4], ["Le Clos Chad", 5], ["Lanza Vini", 6], ["Zirk Kregor", 7], ["Guy James", 8]]
+getWinOdds(e);
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
